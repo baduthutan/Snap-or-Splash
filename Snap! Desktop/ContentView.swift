38cc0ebfd39d2a00681e3d01e27d81e690d90085
vinchen <vinchen996@gmail.com>
2024-05-21 14:14:25 +0700

    Initial Commit

---
+++ Snap! Desktop/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Snap! Desktop
+//
+//  Created by vin chen on 21/05/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Snap! Desktop/Snap__DesktopApp.swift
@@ -0,0 +1,17 @@
+//
+//  Snap__DesktopApp.swift
+//  Snap! Desktop
+//
+//  Created by vin chen on 21/05/24.
+//
+
+import SwiftUI
+
+@main
+struct Snap__DesktopApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
